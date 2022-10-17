37d3a16f22d6abaa38b03f0371fbdb5f1a9895d0
aleksandr_sa <coldsun93@mail.ru>
2022-10-17 21:33:38 +0300

    add Task01 and Task02

---
+++ Task01/Program.cs
@@ -0,0 +1,50 @@
+﻿// 9. Напишите программу, которая
+// выводит случайное число из отрезка [10, 99] b
+// показывает наибольшую цифру числа.
+
+// 78 -> 8
+// 12-> 2
+// 85 -> 8
+
+// int num = 88; //new Random().Next(10, 100);
+// Console.WriteLine($"Случайное число из диапозона 10-99 -> {num}");
+
+// int firstDigit = num / 10;
+// int secondDigit = num % 10;
+
+// if (firstDigit > secondDigit) Console.Write($"Наибольшая цифра числа = {firstDigit}");
+// else Console.Write($"Наибольшая цифра числа = {num} = {secondDigit}");
+
+// int result = firstDigit > secondDigit ? firstDigit : secondDigit;
+
+//int max = Math.Max(firstDigit, secondDigit);
+//Console.Write($"Наибольшая цифра числа = {num} = {max}");
+
+
+// int firstDigit = num / 10; // 78 / 10 = 7
+// int secondDigit = num % 10; // 78 % 10 = 8
+
+// int res = firstDigit;
+// if (secondDigit > firstDigit) res = secondDigit;
+// Console.WriteLine($"Наибольшая цифра числа {num} = {res}");
+
+// int result = firstDigit > secondDigit ? firstDigit : secondDigit;
+// Console.WriteLine($"Наибольшая цифра числа {num} = {result}");
+
+// Console.Write($"Наибольшая цифра числа {num} = ");
+// Console.Write(firstDigit > secondDigit ? firstDigit : secondDigit);
+
+// int MaxDigit(int number)
+// {
+//     int firstDigit = number / 10; // 78 / 10 = 7
+//     int secondDigit = number % 10; // 78 % 10 = 8
+//     return firstDigit > secondDigit ? firstDigit : secondDigit;
+// }
+
+// int num = new Random().Next(10, 100);
+// Console.WriteLine($"Случайное число из диапазона 10 - 99 -> {num}");
+
+// int maxDigit = MaxDigit(num);
+// Console.WriteLine($"Наибольшая цифра числа {num} = {maxDigit}");
+
+
---
+++ Task02/Program.cs
@@ -0,0 +1,38 @@
+﻿// 11. Напишите программу, которая выводит случайное
+// трёхзначное число и удаляет вторую цифру этого
+// числа.
+// 456 -> 46
+// 782 -> 72
+// 918 -> 98
+
+int num = new Random().Next(100, 1000);
+Console.WriteLine($"Случайное число из диапазона 100 - 999 -> {num}");
+
+int FunTwo(int numC)
+{
+    int firstDigit = numC / 100;
+    int threeDigit = numC % 10;
+    return firstDigit * 10 + threeDigit;
+}
+
+Console.WriteLine($"{FunTwo(num)}");
+
+//***********************************************************
+
+// 12. Напишите программу, которая будет принимать на
+// вход два числа и выводить, является ли первое число
+// кратным второму. Если число 1 не кратно числу 2, то
+// программа выводит остаток от деления.
+// 34, 5 -> не кратно, остаток 4
+// 16, 4 -> кратно
+
+// int Ostatok(int number1, int number2)
+// {
+//      return number1 % number2;
+// }
+
+// int numA = Convert.ToInt32(Console.ReadLine());
+// int numB = Convert.ToInt32(Console.ReadLine());
+
+// if (Ostatok(numA, numB) == 0) Console.WriteLine($"число {numA} кратно {numB}");
+// else Console.WriteLine($"число {numA} не кратно {numB}, остаток {Ostatok(numA, numB)}");
