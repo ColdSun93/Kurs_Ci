0c69fe5f4d311534de332ec191d0e14f6bf505ed
aleksandr_sa <coldsun93@mail.ru>
2022-10-22 02:36:55 +0300

    add Homework_23

---
+++ Homework_23/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 23
+// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 5 ->
+// 1 | 1
+// 2 | 8
+// 3 | 27
+// 4 | 64
+// 5 | 125
+
+void Table(int n1)
+{
+    int count = 1;
+    while (count <= n1)
+    {
+        Console.WriteLine($"|{count,4} | {Math.Pow(count, 3),4}|");
+        count++;
+    }
+}
+
+Console.WriteLine("Введите натуральное число N");
+int n = Convert.ToInt32(Console.ReadLine());
+if (n > 0) Table(n);
+else Console.WriteLine("Некорректное значение");
