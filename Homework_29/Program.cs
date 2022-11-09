f4409b1735b4956525d605efb725586a70d485ad
aleksandr_sa <coldsun93@mail.ru>
2022-11-09 19:46:31 +0300

    add Homework_29 add Read Array

--- Homework_29/Program.cs
+++ Homework_29/Program.cs
@@ -1,13 +1,15 @@
 ﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
 // 1, 2, 5, 7, 19, 6, 1, 33 -> [1, 2, 5, 7, 19, 6, 1, 33]
 
-int[] CompletionArr(int len)
+int[] ReadArr(int len)
 {
-    int[] arr = new int [len];
-    Random rnd = new Random();
+    int[] arr = new int[len];
+    //Random rnd = new Random();            //для автоматического заполнения массива
     for (int i = 0; i < len; i++)
     {
-        arr[i] = rnd.Next();
+        Console.Write($"{i + 1} элемент массива = "); // обозначение 0 элемента массива, для пользователя идет с 1
+        //arr[i] = rnd.Next(0, 100);        //для автоматического заполнения массива
+        arr[i] = Convert.ToInt32(Console.ReadLine());
     }
     return arr;
 }
@@ -17,15 +19,15 @@ void WriteArr(int[] arr)
     for (int i = 0; i < arr.Length; i++)
     {
 
-        if (i != arr.Length-1) Console.Write($"{arr[i]}, ");
+        if (i != arr.Length - 1) Console.Write($"{arr[i]}, ");
         else Console.Write($"{arr[i]}");
     }
     Console.Write("]");
 }
 
- Console.WriteLine("Введите размер создаваемого массива");
- int size = Convert.ToInt32(Console.ReadLine());
-int[] array = CompletionArr(size);
-//int[] array = new int [size];
-//CompletionArr(array);
+Console.WriteLine("Введите массив из 8 элементов");
+
+int[] array = ReadArr(8);
+//int[] array = {1, 2, 5, 7, 19, 6, 1, 33};     
+
 WriteArr(array);
